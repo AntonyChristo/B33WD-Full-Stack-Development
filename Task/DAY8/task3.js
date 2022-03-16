54a87fec39e0ee2abaf5f61dec6d96d9584f0f30
AntonyChristo <100108594+AntonyChristo@users.noreply.github.com>
2022-03-16 11:10:17 +0530

    Add files via upload

---
+++ Task/DAY8/task3.js
@@ -0,0 +1,21 @@
+    
+            //3. Write a “person” class to hold all the details.
+
+class person{}
+let personObj= new person();
+personObj.firstname="Antony",
+personObj.lastname="Christo",
+personObj.fullname="Antony Christo";
+personObj.age="22";
+personObj.eyecolor="brown";
+personObj.haircolor="black";
+personObj.height="175";
+personObj.educationalQualification=" Bachelor of Computer Application";
+personObj.work="Arts";
+personObj.address={
+        city:"Thirunager",
+        district:"Madurai",
+        state:"TamilNadu",
+        country:"india",
+    }
+console.log(personObj);
---
+++ Task/DAY8/task4.js
@@ -0,0 +1,10 @@
+
+//4.write a class to calculate uber price.
+
+var taxiFares;
+var taxiFare;
+var costPerKM=8;
+var distancetraveledKM=15;
+var GST=1.50;
+var taxiFares= "taxiFare="+(GST+(costPerKM*distancetraveledKM));
+console.log(taxiFares);
